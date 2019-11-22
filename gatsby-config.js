9a56a321c33e33a52616a1e0d82cd5780e342a15
Matthieu Auger <matthieua@theodo.fr>
2019-11-22 16:01:27 +0100

    Add gallery :picture:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ['@matthieuauger/gatsby-theme-meetup'],
-}
+  plugins: [
+    {
+      resolve: "@matthieuauger/gatsby-theme-meetup"
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `gallery`,
+        path: `${__dirname}/src/images/gallery`
+      }
+    }
+  ]
+};
